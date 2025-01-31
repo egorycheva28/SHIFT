8b546e26768050fdf4877aeeccc97a8436e2dd46
egorycheva28 <egor.masha28@list.ru>
2025-01-31 17:10:39 +0700

    Добавлена связь с кошельком

--- src/main/java/ru/cft/template/entity/User.java
+++ src/main/java/ru/cft/template/entity/User.java
@@ -1,11 +1,8 @@
 package ru.cft.template.entity;
 
-import com.jayway.jsonpath.internal.function.numeric.Min;
 import jakarta.persistence.*;
-//import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.Date;
 import java.util.UUID;
@@ -20,22 +17,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
-    //@Size()
-    //@Min(value = 1, message="n")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "wallet_id", referencedColumnName = "number")
+    private Wallet wallet;
+
     @Column(nullable = false)
     private String lastName;
 
-    //@NotNull
     @Column(nullable = false)
     private String firstName;
 
     private String middleName;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private Long phone;
 
     @Column(unique = true, nullable = false)
-    //@Email(message = "Invalid email")
     private String email;
 
     @Column(nullable = false)
