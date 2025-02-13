da32a0ba2202db7d2013867441c5166454e0c55d
egorycheva28 <egor.masha28@list.ru>
2025-02-13 11:48:40 +0700

    Удалено лишнее

--- src/main/java/ru/cft/template/api/controller/UserController.java
+++ src/main/java/ru/cft/template/api/controller/UserController.java
@@ -1,14 +1,11 @@
 package ru.cft.template.api.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.cft.template.api.dto.user.CreateUserDto;
 import ru.cft.template.api.dto.user.ReturnUserDto;
 import ru.cft.template.api.dto.user.UpdateUserDto;
-import ru.cft.template.core.exception.AuthorizationException;
 import ru.cft.template.core.service.UserService;
 
 import java.util.UUID;
@@ -30,10 +27,6 @@ public class UserController {
     public Object getUserById(@PathVariable(name = "userId") UUID userId,
                               @RequestHeader("Authorization") UUID sessionId) {
 
-        if (sessionId == null) {
-            throw new AuthorizationException("Пользователь не авторизован.");
-        }
-
         return userService.getUserById(userId, sessionId);
     }
 
@@ -41,10 +34,6 @@ public class UserController {
     public ResponseEntity<String> updateUser(@PathVariable(name = "userId") UUID userId,
                                              @RequestBody UpdateUserDto updateUserDto,
                                              @RequestHeader("Authorization") UUID sessionId) {
-
-        if (sessionId == null) {
-            throw new AuthorizationException("Пользователь не авторизован.");
-        }
 
         return userService.updateUser(userId, updateUserDto, sessionId);
     }
--- src/main/java/ru/cft/template/api/controller/WalletController.java
+++ src/main/java/ru/cft/template/api/controller/WalletController.java
@@ -3,7 +3,6 @@ package ru.cft.template.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.cft.template.api.dto.wallet.GetWalletDto;
-import ru.cft.template.core.exception.AuthorizationException;
 import ru.cft.template.core.service.WalletService;
 
 import java.util.UUID;
@@ -18,10 +17,6 @@ public class WalletController {
     @GetMapping("/{userId}")
     public GetWalletDto getWalletByUserId(@PathVariable(name = "userId") UUID userId,
                                           @RequestHeader("Authorization") UUID sessionId) {
-
-        if (sessionId == null) {
-            throw new AuthorizationException("Пользователь не авторизован");
-        }
 
         return walletService.getWalletByUserId(userId, sessionId);
     }
